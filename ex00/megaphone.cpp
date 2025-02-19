0a8ec84228af0b620f65edd7001ff202b146e9f6
fran <user@users-MacBook-Pro.local>
2025-02-19 14:27:46 +0100

    First commit

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,28 @@
+#include <iostream> // Allows me to use cout cin
+#include <cctype> // Allows me to use functions like isalfa or toupper
+
+void  print_elem(char *c) {
+  while (*c != '\0') {
+    if (std::isalpha(*c)) {
+      std::cout << (char)std::toupper(*c);
+    } else {
+      std::cout << *c;
+    }
+    c++;
+  }
+}
+int main(int args, char **argv) {
+
+  if (args == 1) {
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+  } else {
+    argv++;
+    while(*argv) {
+      print_elem(*argv);
+      argv++;
+    }
+    std::cout << std::endl;
+  }
+
+  return 0;
+}
