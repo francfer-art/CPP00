c73c3524314ad250290f6d6b17466ae9a1a5aa03
fran <user@users-MacBook-Pro.local>
2025-02-26 12:32:36 +0100

    Fixed empty strings when adding a contact

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <limits>
+#include <ctype.h>
 
 std::string getValidIndex()
 {
@@ -33,60 +34,72 @@ std::string getValidIndex()
     }
 }
 
-int main()
-{
+bool isValidPhoneNumber(const std::string &phone) {
+    return !phone.empty() && std::all_of(phone.begin(), phone.end(), ::isdigit);
+}
+
+std::string getInput(const std::string &prompt) {
+    std::string input;
+    while (true) {
+        std::cout << prompt;
+        if (!std::getline(std::cin, input)) {
+            std::cout << "\033[1;31mEOF detected. Exiting phonebook...\033[0m" << std::endl;
+            exit(0);
+        }
+        if (!input.empty()) break;
+        std::cout << "\033[1;31mInput cannot be empty. Please try again.\033[0m" << std::endl;
+    }
+    return input;
+}
+
+int main() {
     Phonebook phonebook;
     std::string option;
 
     std::cout << "Welcome to the phonebook!" << std::endl;
 
-    while (true) 
-    {
-      std::cout << "\033[1;36mChoose an option: ADD, SEARCH, EXIT\033[0m" << std::endl;
-        if (!std::getline(std::cin, option))
-        {
-            std::cout << "\033[1;31m" <<"EOF detected. Exiting phonebook..." << "\033[0m" <<std::endl;
+    while (true) {
+        std::cout << "\033[1;36mChoose an option: ADD, SEARCH, EXIT\033[0m" << std::endl;
+        if (!std::getline(std::cin, option)) {
+            std::cout << "\033[1;31mEOF detected. Exiting phonebook...\033[0m" << std::endl;
             break;
         }
 
-        if (option == "EXIT")
-        {
-          std::cout << "\033[1;31m" <<"Exiting phonebook..." << "\033[0m" <<std::endl;
-          break;
+        if (option == "EXIT") {
+            std::cout << "\033[1;31mExiting phonebook...\033[0m" << std::endl;
+            break;
         }
-        else if (option == "ADD")
-        {
+        else if (option == "ADD") {
             std::string firstName, lastName, nickname, phoneNumber, darkestSecret;
 
-            std::cout << "Enter the first name: ";
-            if (!std::getline(std::cin, firstName)) { std::cout << "\033[1;31m" <<"EOF detected. Exiting phonebook..." << "\033[0m" <<std::endl; break; }
+            do {
+                firstName = getInput("Enter the first name: ");
+                lastName = getInput("Enter the last name: ");
+                nickname = getInput("Enter the nickname: ");
 
-            std::cout << "Enter the last name: "; 
-            if (!std::getline(std::cin, lastName)) { std::cout << "\033[1;31m" <<"EOF detected. Exiting phonebook..." << "\033[0m" <<std::endl; break; }
+                do {
+                    phoneNumber = getInput("Enter the phone number: ");
+                    if (!isValidPhoneNumber(phoneNumber)) {
+                        std::cout << "\033[1;31mInvalid phone number. Only digits are allowed.\033[0m" << std::endl;
+                    }
+                } while (!isValidPhoneNumber(phoneNumber));
 
-            std::cout << "Enter the nickname: ";
-            if (!std::getline(std::cin, nickname)) { std::cout << "\033[1;31m" <<"EOF detected. Exiting phonebook..." << "\033[0m" <<std::endl; break; }
+                darkestSecret = getInput("Enter the darkest secret: ");
 
-            std::cout << "Enter the phone number: ";
-            if (!std::getline(std::cin, phoneNumber)) { std::cout << "\033[1;31m" <<"EOF detected. Exiting phonebook..." << "\033[0m" <<std::endl; break; }
-
-            std::cout << "Enter the darkest secret: ";
-            if (!std::getline(std::cin, darkestSecret)) { std::cout << "\033[1;31m" <<"EOF detected. Exiting phonebook..." << "\033[0m" <<std::endl; break; }
+            } while (firstName.empty() || lastName.empty() || nickname.empty() || phoneNumber.empty() || darkestSecret.empty());
 
             phonebook.addContact(firstName, lastName, nickname, phoneNumber, darkestSecret);
+            std::cout << "\033[1;32mContact saved successfully!\033[0m" << std::endl;
         }
-        else if (option == "SEARCH")
-        {
+        else if (option == "SEARCH") {
             std::string index = getValidIndex();
             phonebook.displayContact(std::stoi(index));
         }
-        else if (option == "DISPLAY")
-        {
+        else if (option == "DISPLAY") {
             phonebook.displayContacts();
         }
-        else
-        {
-          std::cout << "\033[1;31mInvalid option\033[0m" << std::endl;
+        else {
+            std::cout << "\033[1;31mInvalid option\033[0m" << std::endl;
         }
     }
 
